e21e3f4d279de3cf1fff7da7e43462d140ebcd68
metropole.paulista123456789 <metropole.paulista123456789@gmail.com>
2025-03-26 23:24:31 +0000

    primeiro commit do js

---
+++ main.js
@@ -0,0 +1,11 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfpeople = 0
+
+function receiveBillValue(){
+    Bill = document.querySelector("#bill").ValueAsNumber
+}
+
+function receiveNumberOfpeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
