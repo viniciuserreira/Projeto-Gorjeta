4e02112809b14c2cc43ec61164ca02a2caa0d377
metropole.paulista123456789 <metropole.paulista123456789@gmail.com>
2025-03-27 22:31:03 +0000

    quarto commit do js

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ let buttonSelected = null
 
 function receiveBillValue(){
     Bill = document.querySelector("#bill").ValueAsNumber
+    calculate()
 }
 
 function receiveNumberOfpeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
 function receiveTipPercentageValue(value){
@@ -27,6 +29,7 @@ function receiveTipPercentageValue(value){
 
    buttonSelected = document.querySelector(`#button-${value}`)
    buttonSelected.classList.add("button-selected")
+   calculate()
 }
 
 function receiveCustomTipPercentageValue(){
@@ -36,4 +39,19 @@ function receiveCustomTipPercentageValue(){
     }
 
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+    calculate()
+}
+
+function calculate (){
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+       let amountStrong = document.querySelector(".amount strong")
+       let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+       amountStrong.innerText = '$${tipAmountPerson.toFixed(2)}'
+     
+        let totalStrong = document.querySelector(".total strong")
+        let totalPerson = bill / numberOfpeople + tipAmountPerson
+        totalStrong.innerText = '$${totalPerson.toFixed(2)}'
+    } else {
+        console.log("preencha tudo")
+    }
 }
