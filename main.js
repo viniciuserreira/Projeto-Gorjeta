6ab7107b7bfd386cb39feb44186e2e7d5de554a6
metropole.paulista123456789 <metropole.paulista123456789@gmail.com>
2025-03-27 21:30:14 +0000

    segundo commit do css

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfpeople = 0
+let buttonSelected = null
 
 function receiveBillValue(){
     Bill = document.querySelector("#bill").ValueAsNumber
@@ -8,4 +9,16 @@ function receiveBillValue(){
 
 function receiveNumberOfpeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+function receiveTipPercentageValue(value){
+    if (buttonSelected !== null) {
+        buttonSelected.classList.remove("button-selected")
+    }
+
+   tipPercentage = value / 100
+    console.log(tipPercentage)
+
+   buttonSelected = document.querySelector(`#button-${value}`)
+   buttonSelected.classList.add("button-selected")
 }
